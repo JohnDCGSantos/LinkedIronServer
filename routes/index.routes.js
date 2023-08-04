9c325398200272dfdfa1553e052e4fcb99166ef6
JohnDCGSantos <jdcg.santos@gmail.com>
2023-08-04 16:36:08 +0100

    Routes waiting for UserModel

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,5 +3,7 @@ const router = require('express').Router()
 router.get('/', (req, res, next) => {
   res.json('LinkedIron')
 })
+const usersRoutes = require('./users.routes')
+router.use('/users', usersRoutes)
 
 module.exports = router
