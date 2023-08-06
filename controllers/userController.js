58bbfc90f9ad0db5f62fbeafe363114f70d12893
JohnDCGSantos <jdcg.santos@gmail.com>
2023-08-06 16:49:15 +0100

    merging

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,65 +1,62 @@
-const User = require("../models/User.model");
+const User = require('../models/User.model')
 
 /**** Fetch all Users ****/
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
-    res.status(200).json(users);
+    const users = await User.find()
+    res.status(200).json(users)
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occured while fetching the user profile." });
+    res.status(500).json({ error: 'An error occured while fetching the user profile.' })
   }
-};
+}
 
 /**** Fetch User Profile ****/
-const getUserProfile = async(req, res)=>{
-    try {
-        const userProfile = await User.findById(req.params.userId)
-        if (!userProfile) {
-          return res.status(404).json({ errorMessage: 'User profile not found' })
-        }
-        res.status(200).json(userProfile)
-        console.log(userProfile)
-      } catch (error) {
-        console.log(error)
-        res.status(500).json(error)
-      }
+const getUserProfile = async (req, res) => {
+  try {
+    const userProfile = await User.findById(req.params.userId)
+    if (!userProfile) {
+      return res.status(404).json({ errorMessage: 'User profile not found' })
     }
+    res.status(200).json(userProfile)
+    console.log(userProfile)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
 
 /****Update User Data ****/
 
-const updateUserData = async(req, res)=>{
-    try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.userId, req.body, {
-          new: true,
-        })
-    
-        res.json(updatedUser)
-        console.log(updateUser)
-      } catch (error) {
-        console.log(error)
-        res.status(500).json(error)
-      }
-    };
+const updateUserData = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.userId, req.body, {
+      new: true,
+    })
 
-    /****Delete User Data ****/
+    res.json(updatedUser)
+    console.log(updateUser)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
 
-    const deleteUser=async() =>{
-        try {
-            await User.findByIdAndDelete(req.params.userId)
-        
-            res.status(202).json({ message: 'User successfully deleted' })
-          } catch (error) {
-            console.log(error)
-            res.status(500).json(error)
-          }
-        };
-    
+/****Delete User Data ****/
 
-module.exports={
-    getUsers,
-    getUserProfile,
-    updateUserData,
-    deleteUser
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.userId)
+
+    res.status(202).json({ message: 'User successfully deleted' })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+module.exports = {
+  getUsers,
+  getUserProfile,
+  updateUserData,
+  deleteUser,
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,16 @@
 const router = require('express').Router()
-const {signUp, login}= require ('../controllers/authenticationController')
-const { isAuthenticated } = require('../middlewares/jwt.middleware');
+const { signUp, login } = require('../controllers/authenticationController')
+const { isAuthenticated } = require('../middlewares/jwt.middleware')
 
 router.get('/', (req, res, next) => {
   res.json('All good in auth')
 })
 
 /* POST route to signup */
-router.post('/signup', signUp);
+router.post('/signup', signUp)
 
 /* POST route to login */
-router.post('/login', login);
+router.post('/login', login)
 
 /* GET route to verify the token */
 router.get('/verify', isAuthenticated, (req, res) => {
